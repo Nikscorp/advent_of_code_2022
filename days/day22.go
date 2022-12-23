e8105b7047d4ffd45f0c656466a9c6e553777fef
Nikita Voynov <voynov@nikscorp.com>
2022-12-23 19:06:38 +0400

    day 22 done

--- days/day22.go
+++ days/day22.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 )
 
+type coordWithDir struct {
+	c   coord
+	dir coord
+}
+
 func Day22() int {
 	file, _ := os.Open("input/input22.txt")
 	defer file.Close()
@@ -74,13 +79,164 @@ func Day22() int {
 	}
 	dirInd := 0
 	cur := start
-	printGrid(grid)
-	for i := 0; i < len(grid[11]); i++ {
-		fmt.Println(i, string(grid[11][i]))
-	}
-	fmt.Println(string(grid[11]))
 
-	for _, cmd := range cmdS {
+	someMap := make(map[coordWithDir]coordWithDir)
+	// 2 -> 5
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{49, 0 + i},
+			dir: coord{-1, 0},
+		}] = coordWithDir{
+			c:   coord{0, 149 - i},
+			dir: coord{1, 0},
+		}
+	}
+
+	// 6 -> 4
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{50, 150 + i},
+			dir: coord{1, 0},
+		}] = coordWithDir{
+			c:   coord{50 + i, 149},
+			dir: coord{0, -1},
+		}
+	}
+
+	// 4 -> 6
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{50 + i, 150},
+			dir: coord{0, 1},
+		}] = coordWithDir{
+			c:   coord{49, 150 + i},
+			dir: coord{-1, 0},
+		}
+	}
+
+	// 5 -> 3
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{0 + i, 99},
+			dir: coord{0, -1},
+		}] = coordWithDir{
+			c:   coord{50, 50 + i},
+			dir: coord{1, 0},
+		}
+	}
+
+	// 3 -> 5
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{49, 50 + i},
+			dir: coord{-1, 0},
+		}] = coordWithDir{
+			c:   coord{0 + i, 100},
+			dir: coord{0, 1},
+		}
+	}
+
+	// 4 -> 1
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{100, 100 + i},
+			dir: coord{1, 0},
+		}] = coordWithDir{
+			c:   coord{149, 49 - i},
+			dir: coord{-1, 0},
+		}
+	}
+
+	// 1 -> 4
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{150, 0 + i},
+			dir: coord{1, 0},
+		}] = coordWithDir{
+			c:   coord{99, 149 - i},
+			dir: coord{-1, 0},
+		}
+	}
+
+	// 1 -> 6
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{100 + i, -1},
+			dir: coord{0, -1},
+		}] = coordWithDir{
+			c:   coord{0 + i, 199},
+			dir: coord{0, -1},
+		}
+	}
+
+	// 2 -> 6
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{50 + i, -1},
+			dir: coord{0, -1},
+		}] = coordWithDir{
+			c:   coord{0, 150 + i},
+			dir: coord{1, 0},
+		}
+	}
+
+	// 6 -> 1
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{0 + i, 200},
+			dir: coord{0, 1},
+		}] = coordWithDir{
+			c:   coord{100 + i, 0},
+			dir: coord{0, 1},
+		}
+	}
+
+	// 6 -> 2
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{-1, 150 + i},
+			dir: coord{-1, 0},
+		}] = coordWithDir{
+			c:   coord{50 + i, 0},
+			dir: coord{0, 1},
+		}
+	}
+
+	// 5 -> 2
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{-1, 100 + i},
+			dir: coord{-1, 0},
+		}] = coordWithDir{
+			c:   coord{50, 49 - i},
+			dir: coord{1, 0},
+		}
+	}
+
+	// 3 -> 1
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{100, 50 + i},
+			dir: coord{1, 0},
+		}] = coordWithDir{
+			c:   coord{100 + i, 49},
+			dir: coord{0, -1},
+		}
+	}
+
+	// 1 -> 3
+	for i := 0; i < 50; i++ {
+		someMap[coordWithDir{
+			c:   coord{100 + i, 50},
+			dir: coord{0, 1},
+		}] = coordWithDir{
+			c:   coord{99, 50 + i},
+			dir: coord{-1, 0},
+		}
+	}
+
+	for i, cmd := range cmdS {
+		fmt.Println(i, "of", len(cmdS))
 		if cmd == "R" {
 			dirInd = (dirInd + 1) % len(dirs)
 			continue
@@ -106,12 +262,17 @@ func Day22() int {
 				// 	}
 				// 	newCoord = tmpCoord
 				// }
-				promt := fmt.Sprintf("i am at (%d, %d). Going to (%d, %d). Where to go?\n", newCoord.x, newCoord.y, dir.x, dir.y)
-				fmt.Println(promt)
 				tmpDir := coord{}
-				_, err := fmt.Scanf("%d %d %d %d\n", &newCoord.x, &newCoord.y, &tmpDir.x, &tmpDir.y)
-				if err != nil {
-					panic(err)
+				if v, ok := someMap[coordWithDir{newCoord, dir}]; ok {
+					newCoord = v.c
+					tmpDir = v.dir
+				} else {
+					promt := fmt.Sprintf("i am at (%d, %d). Going to (%d, %d). Where to go?\n", newCoord.x, newCoord.y, dir.x, dir.y)
+					fmt.Println(promt)
+					_, err := fmt.Scanf("%d %d %d %d\n", &newCoord.x, &newCoord.y, &tmpDir.x, &tmpDir.y)
+					if err != nil {
+						panic(err)
+					}
 				}
 				if grid[newCoord.y][newCoord.x] == '#' {
 					break
--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 )
 
 func main() {
-	fmt.Println(days.Day23())
+	fmt.Println(days.Day22())
 }
